6bfb188b92544abc85b3985bcb13843622fa4d20
Krzysztof Siwek <siwyus@gmail.com>
2019-12-04 12:43:12 +0100

    Add Navbar.js and install SASS

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
+import Home from "./components/pages/Home";
+import About from "./components/pages/About";
+import Projects from "./components/pages/Projects";
+import Navbar from "./components/layout/Navbar";
 
-function App() {
-  return <div className="App">Hello World!</div>;
-}
+const App = () => {
+  return (
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/projects">
+            <Projects />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+};
 
 export default App;
---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <ul>
+        <li>
+          <Link className="link" to="/">
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link className="link" to="/about">
+            About
+          </Link>
+        </li>
+        <li>
+          <Link className="link" to="/projects">
+            Projects
+          </Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
